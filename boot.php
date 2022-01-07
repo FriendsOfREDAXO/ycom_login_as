e408abd8e92c714dd261ef266aa7779e82539d49
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-07 21:16:05 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -2,8 +2,6 @@
 
 // Working draft.
 
-
-
  rex_extension::register('YCOM_AUTH_USER_CHECK', function ($ep) {
         // backendnutzer bei jedem seitenaufruf einloggen (wg ycom benutzerwechsel)
         if (rex_backend_login::hasSession() && $beUser = rex_backend_login::createUser()) {
@@ -16,7 +14,7 @@
                 else {
                 $user_id = 50;
                 }
-                if ($user_id >= 1 && !rex_ycom_auth::getUser()) {
+                if (($user_id >= 1 && !rex_ycom_auth::getUser()) || $user_id_link && rex_ycom_auth::getUser() && $ycom_user->id != $user_id_link ) {
                     $ycom_user = rex_ycom_auth::loginWithParams(['id' => $user_id]);
                     if ($ycom_user) {
                         return true;

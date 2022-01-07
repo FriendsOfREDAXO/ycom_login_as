ade95e580df3b7b7e45b7a573641efda0732b2fe
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-07 21:10:29 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -2,17 +2,26 @@
 
 // Working draft.
 
-rex_extension::register('YCOM_AUTH_USER_CHECK', function ($ep) {
-    // backendnutzer bei jedem seitenaufruf einloggen (wg ycom benutzerwechsel)
-    if (rex_backend_login::hasSession() && $beUser = rex_backend_login::createUser()) {
-        if ($beUser->isAdmin() || $beUser->hasPerm('permdafuer[]')) {
-            $user_id = rex_get('user_id', 'int', 0);
-            if ($user_id) {
-                $ycom_user = rex_ycom_auth::loginWithParams(['id' => $user_id]);
-                if ($ycom_user) {
-                    return true;
+
+
+ rex_extension::register('YCOM_AUTH_USER_CHECK', function ($ep) {
+        // backendnutzer bei jedem seitenaufruf einloggen (wg ycom benutzerwechsel)
+        if (rex_backend_login::hasSession() && $beUser = rex_backend_login::createUser()) {
+            if ($beUser->isAdmin() || $beUser->hasPerm('ycom[]')) {
+                $user_id_link = rex_get('ycom_user_id', 'int', 0);
+                if ($user_id_link)
+                {
+                  $user_id = $user_id_link;   
+                }
+                else {
+                $user_id = 50;
+                }
+                if ($user_id >= 1 && !rex_ycom_auth::getUser()) {
+                    $ycom_user = rex_ycom_auth::loginWithParams(['id' => $user_id]);
+                    if ($ycom_user) {
+                        return true;
+                    }
                 }
             }
         }
-    }
-});
+    });

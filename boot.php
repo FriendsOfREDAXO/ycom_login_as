46784260413edcc59bd2961ec21898a46d5df5ec
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-08 00:08:05 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,18 +1,16 @@
 <?php 
-$addon = rex_addon::get('ycom_impersonate');
-$default_id = (int) $addon->getConfig('default_id');
 // Working draft.
 rex_extension::register('YCOM_AUTH_USER_CHECK', function ($ep) {
     // backendnutzer bei jedem seitenaufruf einloggen (wg ycom benutzerwechsel)
     if (rex_backend_login::hasSession() && $beUser = rex_backend_login::createUser()) {
         if ($beUser->isAdmin() || $beUser->hasPerm('ycom[]')) {
+			$addon = rex_addon::get('ycom_impersonate');
+			$default_id = (int) $addon->getConfig('default_id');
             $user_id_link = rex_get('ycom_user_id', 'int', 0);
-            if ($user_id_link)
+			$user_id = $default_id;
+            if ($user_id_link > 0)
             {
                 $user_id = $user_id_link;   
-            }
-            else {
-                $user_id = $default_id;
             }
             if (($user_id >= 1 && !rex_ycom_auth::getUser()) || $user_id_link && rex_ycom_auth::getUser() && $ycom_user->id != $user_id_link ) {
 
@@ -34,5 +32,6 @@ if (rex::isBackend() && rex_request('table_name') == 'rex_ycom_user') {
         }); 
     });
 }
+
 
 

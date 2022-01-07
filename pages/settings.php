7ea391f11f6f856a1570ec922e656d809a60a06a
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-07 23:33:00 +0100

    Update settings.php

--- pages/settings.php
+++ pages/settings.php
@@ -1,8 +1,18 @@
 <?php
 $addon = rex_addon::get('ycom_impersonate');
 $form = rex_config_form::factory($addon->name);
-$field = $form->addInputField('text', 'default_id', null, ["class" => "form-control"]);
+
+$field = $form->addSelectField('default_id', null, ['class' => 'form-control']);
+$field->setAttribute('class', 'form-control selectpicker');
+$field->setAttribute('"data-live-search', 'true');
 $field->setLabel($addon->i18n('ycom_impersonate_default_id'));
+$select = $field->getSelect();
+$select->setSize(1);
+$mSql = rex_sql::factory();
+        foreach ($mSql->getArray('SELECT id, firstname, name FROM ' . rex::getTablePrefix() . 'ycom_user ORDER BY name') as $m) {
+            $select->addOption($m['firstname'].' '.$m['name'], (int) $m['id']);
+}
+$field->setNotice($addon->i18n('ycom_impersonate_default_id_notice'));
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);

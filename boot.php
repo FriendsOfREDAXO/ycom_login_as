7d59e11fc719f13bb6e5d9b28d191c729626312a
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-07 17:23:27 +0100

    Working draft

--- boot.php
+++ boot.php
@@ -1,1 +1,18 @@
-<?php // Boot code
+<?php 
+
+// Working draft.
+
+rex_extension::register('YCOM_AUTH_USER_CHECK', function ($ep) {
+    // backendnutzer bei jedem seitenaufruf einloggen (wg ycom benutzerwechsel)
+    if (rex_backend_login::hasSession() && $beUser = rex_backend_login::createUser()) {
+        if ($beUser->isAdmin() || $beUser->hasPerm('permdafuer[]')) {
+            $user_id = rex_get('user_id', 'int', 0);
+            if ($user_id) {
+                $ycom_user = rex_ycom_auth::loginWithParams(['id' => $user_id]);
+                if ($ycom_user) {
+                    return true;
+                }
+            }
+        }
+    }
+});

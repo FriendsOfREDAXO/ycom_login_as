2139ec82a8a3e02aaf5da9e949afffeb2d827011
Thomas Skerbis <thomas.skerbis@mac.com>
2023-11-28 16:22:42 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,4 @@
 <?php
-echo rex_view::title($this->i18n('ycom_impersonate_title'));
+echo rex_view::title($this->i18n('ycom_login_as_title'));
 $subpage = rex_be_controller::getCurrentPagePart(2);
 rex_be_controller::includeCurrentPageSubPath();
